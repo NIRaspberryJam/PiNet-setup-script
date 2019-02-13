c7fb73faa7a3e538790850878c034151e9e6b990
Andrew Mulholland <gbaman@users.noreply.github.com>
2019-02-13 23:10:18 +0000

    Add gpiozero

--- setup_jam.sh
+++ setup_jam.sh
@@ -1,5 +1,5 @@
 # Setup script for PiNet to add all required software for NI Raspberry Jam
 
-sudo apt-get install gimp pv python3-matplotlib
+sudo apt-get install gimp pv python3-matplotlib python3-gpiozero
 sudo pip3 install network twython numpy
 
