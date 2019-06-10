14c570e0dd93c2177dec8e9e924b7c6c99b89a90
gbaman <gbaman@users.noreply.github.com>
2019-06-11 00:04:23 +0100

    Fix duplicates bug

--- pinet_nijis_uploader/pinet_nijis_uploader.py
+++ pinet_nijis_uploader/pinet_nijis_uploader.py
@@ -4,10 +4,12 @@ import json
 from secret import api_key
 
 users = pwd.getpwall()
-usernames = []
+usernames = set()
 for user in users:
     if user.pw_uid > 1000:
-        usernames.append(user.pw_name)
+        usernames.add(user.pw_name.lower())
+
+usernames = list(usernames)
 
 data = {"usernames": usernames, "token":api_key}
 requests.post("https://workshops.niraspberryjam.com/api/upload_pinet_usernames", json=json.dumps(data))
--- setup_jam.sh
+++ setup_jam.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Setup script for PiNet to add all required software for NI Raspberry Jam
 
-sudo apt-get install gimp pv python3-matplotlib python3-gpiozero
-sudo pip3 install network twython numpy
+sudo ltsp-chroot --arch armhf apt install gimp pv python3-matplotlib python3-gpiozero
+sudo ltsp-chroot --arch armhf pip3 install network twython numpy
 
 mkdir /opt/pinet_nijis_uploader
 wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader.py -O /opt/pinet_nijis_uploader/pinet_nijis_uploader.py
@@ -11,7 +11,8 @@ chmod +x /opt/pinet_nijis_uploader/pinet_nijis_uploader.sh
 rm /etc/cron.d/pinet_nijis_uploader
 wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader_cron -O /etc/cron.d/pinet_nijis_uploader_cron
 if ! [[ -f "/opt/pinet_nijis_uploader/secret.py" ]]; then 
-    cat "api_key = \"\"" > "/opt/pinet_nijis_uploader/secret.py"
+    echo "api_key = \"\"" > "/opt/pinet_nijis_uploader/secret.py"
     nano /opt/pinet_nijis_uploader/secret.py
 
 fi
+service cron reload
