6a1aca6f21a4380642d8f2108309af7a8a46502c
gbaman <gbaman@users.noreply.github.com>
2019-06-10 23:29:06 +0100

    Rename cron file

--- setup_jam.sh
+++ setup_jam.sh
@@ -5,11 +5,11 @@ sudo apt-get install gimp pv python3-matplotlib python3-gpiozero
 sudo pip3 install network twython numpy
 
 mkdir /opt/pinet_nijis_uploader
-wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader.py /opt/pinet_nijis_uploader/pinet_nijis_uploader.py
-wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader.sh /opt/pinet_nijis_uploader/pinet_nijis_uploader.sh
+wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader.py -O /opt/pinet_nijis_uploader/pinet_nijis_uploader.py
+wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader.sh -O /opt/pinet_nijis_uploader/pinet_nijis_uploader.sh
 chmod +x /opt/pinet_nijis_uploader/pinet_nijis_uploader.sh
 rm /etc/cron.d/pinet_nijis_uploader
-wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader_cron /etc/cron.d/pinet_nijis_uploader_cron
+wget https://raw.githubusercontent.com/NIRaspberryJam/PiNet-setup-script/master/pinet_nijis_uploader/pinet_nijis_uploader_cron -O /etc/cron.d/pinet_nijis_uploader_cron
 if ! [[ -f "/opt/pinet_nijis_uploader/secret.py" ]]; then 
     cat "api_key = \"\"" > "/opt/pinet_nijis_uploader/secret.py"
     nano /opt/pinet_nijis_uploader/secret.py
